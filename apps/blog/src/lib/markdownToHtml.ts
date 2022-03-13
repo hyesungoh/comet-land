e45a8913929d11faca0e118d5e07eac2716e7c2c
Hyesung Oh <haesungoh414@gmail.com>
2022-03-14 00:49:33 +0900

    feat: using remark-prism for code highlight

--- apps/blog/src/lib/markdownToHtml.ts
+++ apps/blog/src/lib/markdownToHtml.ts
@@ -1,7 +1,11 @@
 import { remark } from 'remark';
 import html from 'remark-html';
+import prism from 'remark-prism';
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  const result = await remark()
+    .use(html, { sanitize: false })
+    .use(prism, { plugins: ['line-numbers', 'autolinker', 'diff-highlight', 'inline-color'] })
+    .process(markdown);
   return result.toString();
 }
--- apps/blog/src/pages/index.tsx
+++ apps/blog/src/pages/index.tsx
@@ -2,23 +2,11 @@ import { getAllPosts } from '../lib/api';
 import PostType from '../types/post';
 import markdownToHtml from '../lib/markdownToHtml';
 
-export async function getStaticProps() {
-  const allPosts = getAllPosts(['title', 'date', 'content']);
-
-  const postContent = await markdownToHtml(allPosts[0].content);
-  const post = { ...allPosts[0], content: postContent };
-
-  return {
-    props: { allPosts: [post, post] },
-  };
-}
-
 interface Props {
   allPosts: PostType[];
 }
 
 function Blog({ allPosts }: Props) {
-  console.log(allPosts);
   return (
     <div>
       <h1>blog</h1>
@@ -34,3 +22,14 @@ function Blog({ allPosts }: Props) {
 }
 
 export default Blog;
+
+export async function getStaticProps() {
+  const allPosts = getAllPosts(['title', 'date', 'content']);
+
+  const postContent = await markdownToHtml(allPosts[0].content);
+  const post = { ...allPosts[0], content: postContent };
+
+  return {
+    props: { allPosts: [post, post] },
+  };
+}
