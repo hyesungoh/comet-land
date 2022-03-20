df498761a56b6d8e72800f017ba9535a8d0e1354
Hyesung Oh <haesungoh414@gmail.com>
2022-03-21 03:36:43 +0900

    fix: not using getLocaleDate for build test

--- apps/blog/src/constants/KbarActions/posts.ts
+++ apps/blog/src/constants/KbarActions/posts.ts
@@ -1,5 +1,5 @@
 import IconActionType from './type';
-import { getLocalDate } from '../../utils/date';
+// import { getLocalDate } from '../../utils/date';
 import manifest from '../../../_content/manifest.json';
 
 const { posts } = manifest;
@@ -10,7 +10,7 @@ posts.forEach(post => {
   postActions.push({
     id: post.slug,
     name: post.title,
-    subtitle: getLocalDate(post.date),
+    // subtitle: getLocalDate(post.date),
     parent: 'posts',
     icon: 'ChevronRight',
   });
