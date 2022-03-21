3a0d952c95e415a9bfbdac54895a5079e01c2ff8
Hyesung Oh <haesungoh414@gmail.com>
2022-03-22 06:14:58 +0900

    feat: apply gzip

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -1,8 +1,17 @@
 const withTM = require('next-transpile-modules')(['core']);
+const CompressionPlugin = require('compression-webpack-plugin');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = withTM(nextConfig);
+module.exports = withTM(
+  {
+    webpack(config) {
+      const plugins = [...config.plugins, new CompressionPlugin()];
+      return { ...config, plugins };
+    },
+  },
+  nextConfig
+);
