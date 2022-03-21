7eb0852458bc9fedd5edef72cb69661c2f69bc2a
Hyesung Oh <haesungoh414@gmail.com>
2022-03-22 02:34:05 +0900

    refactor: replace content manifest script to scripts directory

--- packages/core/constants/General/index.ts
+++ packages/core/constants/General/index.ts
@@ -4,7 +4,7 @@ export const authorName = 'hyesungoh';
 
 export const blogRepoUrl = 'hyesungoh/comet-land';
 
-export const blogUrl = 'https://www.hyesungoh.xyz';
+export const blogUrl = 'https://www.comet-land-blog.vercel.app';
 
 // Write ever you want :D, default is github
 // 직접 작성해주셔도 좋습니다. 기본적으로 깃허브 주소를 사용합니다.
