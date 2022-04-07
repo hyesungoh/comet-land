4e766e2c9fe799c3de200a2644d3913b723b40a4
Hyesung Oh <haesungoh414@gmail.com>
2022-04-08 02:38:38 +0900

    docs: apply cypress badge at readme

--- packages/config/eslint-nextjs.js
+++ packages/config/eslint-nextjs.js
@@ -62,16 +62,11 @@ module.exports = {
       env: {
         jest: true,
       },
-      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(sepc|test).[jt]s?(x)'],
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(test).[jt]s?(x)'],
       extends: ['plugin:testing-library/react', 'plugin:jest/recommended'],
       rules: {
         'import/no-extraneous-dependencies': ['off', { devDependencies: ['**/?(*.)+(spec|test).[jt]s?(x)'] }],
       },
-    },
-  ],
-  overrides: [
-    {
-      files: ['cypress/intergration/*.[jt]s?(x)'],
     },
   ],
   ignorePatterns: ['**/*.js', '**/*.json', 'node_modules', 'public', 'styles', '.next', 'coverage', 'dist', '.turbo'],
