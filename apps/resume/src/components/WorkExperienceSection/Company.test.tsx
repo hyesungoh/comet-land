fb473dc85014a6324b445d8be950eb69d27e493a
Hyesung Oh <haesungoh414@gmail.com>
2022-04-18 15:31:34 +0900

    test: resume - company components

---
+++ apps/resume/src/components/WorkExperienceSection/Company.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import Company from './Company';
+
+const mockData = {
+  name: 'company name',
+  position: 'position name',
+  startDate: '2020.01',
+  endDate: '2021.01',
+  description: 'description data',
+};
+
+describe('resume - components - Company', () => {
+  it('should render all props', () => {
+    render(<Company {...mockData} />);
+    expect(screen.getByText(mockData.name)).toBeInTheDocument();
+    expect(screen.getByText(mockData.position)).toBeInTheDocument();
+    expect(screen.getByText(`${mockData.startDate} ~ ${mockData.endDate}`)).toBeInTheDocument();
+    expect(screen.getByText(mockData.description)).toBeInTheDocument();
+  });
+
+  it('should sticky', () => {
+    render(<Company {...mockData} />);
+    expect(screen.getByTestId('wrapper')).toHaveStyle('position: sticky;');
+  });
+});
