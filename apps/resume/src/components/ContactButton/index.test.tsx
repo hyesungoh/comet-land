eea41041eb497d03a1b95ff82a68eac315e631ec
Hyesung Oh <haesungoh414@gmail.com>
2022-04-20 22:03:38 +0900

    test: apply test at ContactButton index

---
+++ apps/resume/src/components/ContactButton/index.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import ContactButton from './ContactButton';
+import Default from './index';
+
+describe('resume - components - ContactButton index', () => {
+  it('should render same things with ContactButton component', () => {
+    const { container: defaultContainer } = render(<Default />);
+    const { container } = render(<ContactButton />);
+    expect(defaultContainer).toEqual(container);
+  });
+});
