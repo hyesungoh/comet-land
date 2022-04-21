97fcbe00d782c033cbb759afd47a4df690ded373
Hyesung Oh <haesungoh414@gmail.com>
2022-04-21 22:27:33 +0900

    test: apply test at Resume Li component

---
+++ apps/resume/src/components/Li/Li.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NextUITheme } from '@nextui-org/react';
+import { render, screen } from '@testing-library/react';
+
+import Li from './Li';
+
+const mockTheme = {
+  colors: {
+    primary: {
+      value: 'blue',
+    },
+  },
+};
+
+const mockText = 'list';
+
+describe('resume - components - Li', () => {
+  it('should render childrend text', () => {
+    render(<Li theme={mockTheme as NextUITheme}>{mockText}</Li>);
+    expect(screen.getByText(mockText)).toBeInTheDocument();
+  });
+});
