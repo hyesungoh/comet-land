d13170b981a6f7019fa7072c4b05492120447db5
Hyesung Oh <haesungoh414@gmail.com>
2022-04-21 22:34:42 +0900

    test: apply more test at Resume Header component

--- apps/resume/src/components/Header/Header.test.tsx
+++ apps/resume/src/components/Header/Header.test.tsx
@@ -6,10 +6,15 @@ import Header from './Header';
 const mockHeading = 'heading';
 const mockDescription = 'description';
 
-describe('resume - component - Header', () => {
-  it('should', () => {
+describe('resume - components - Header', () => {
+  it('should render props', () => {
     render(<Header heading={mockHeading} description={mockDescription} />);
     expect(screen.getByText(mockHeading)).toBeInTheDocument();
     expect(screen.getByText(mockDescription)).toBeInTheDocument();
+  });
+
+  it('should render images with alt text', () => {
+    render(<Header heading={mockHeading} description={mockDescription} />);
+    expect(screen.getByRole('img').hasAttribute('alt')).toBeTruthy();
   });
 });
