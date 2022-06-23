4c859ccb2a8cefa8605beda1b4b4daa1bad220f7
Hyesung Oh <haesungoh414@gmail.com>
2022-06-24 00:44:39 +0900

    test: apply more test at DateAndCategoryLink component

--- apps/blog/src/components/DateAndCategoryLink/DateAndCategoryLink.test.tsx
+++ apps/blog/src/components/DateAndCategoryLink/DateAndCategoryLink.test.tsx
@@ -7,8 +7,17 @@ const DATE = '2022-04-14';
 const CATEGORY = 'Guide';
 
 describe('blog - component - DateAndCategoryLink', () => {
-  it('should', () => {
+  it('should defined', () => {
+    expect(DateAndCategoryLink).toBeDefined();
+  });
+
+  it('should render category props', () => {
     render(<DateAndCategoryLink date={DATE} category={CATEGORY} />);
     expect(screen.getByText(CATEGORY)).toBeInTheDocument();
+  });
+
+  it('should render only date withou category', () => {
+    render(<DateAndCategoryLink date={DATE} />);
+    expect(screen.getByText(DATE)).toBeInTheDocument();
   });
 });
