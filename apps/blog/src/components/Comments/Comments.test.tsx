1b64e34483eee83eabad41198054fee982fb06dc
Hyesung Oh <haesungoh414@gmail.com>
2022-07-01 03:03:57 +0900

    test: blog COmments component

--- apps/blog/src/components/Comments/Comments.test.tsx
+++ apps/blog/src/components/Comments/Comments.test.tsx
@@ -1,16 +1,39 @@
-import { render, screen } from '@testing-library/react';
-import { executionAsyncResource } from 'async_hooks';
+import React from 'react';
+import { render } from '@testing-library/react';
 
 import Comments from './Comments';
 
-describe('dd', () => {
-  it('dd', () => {
-    const createElement = jest.spyOn('document', 'createElement');
+describe('blog - components - Comments', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
 
+  it('should defined', () => {
+    expect(Comments).toBeDefined();
+  });
+
+  it('should create script tag', () => {
+    jest.spyOn(document, 'createElement');
     render(<Comments />);
+    expect(document.createElement).lastCalledWith('script');
+  });
 
-    expect(createElement).toBeCalledWith('script');
+  it('should useRef called once', () => {
+    const useRefSpy = jest.spyOn(React, 'useRef');
+    render(<Comments />);
+    expect(useRefSpy).toBeCalledTimes(1);
+  });
 
-    expect(1).toBe(1);
+  it('should check html theme', () => {
+    const querySelectorSpy = jest.spyOn(document, 'querySelector');
+    render(<Comments />);
+    expect(querySelectorSpy).toHaveBeenCalledWith('html');
+  });
+
+  it('should check UTTERANCE already rendered', () => {
+    const UTTERANCE_QUERY = '.utterances-frame';
+    const querySelectorSpy = jest.spyOn(document, 'querySelector');
+    render(<Comments />);
+    expect(querySelectorSpy).toHaveBeenCalledWith(UTTERANCE_QUERY);
   });
 });
