4c0f21b50b975cd89a16f7ad20fa555889ad010a
Hyesung Oh <haesungoh414@gmail.com>
2022-07-02 09:47:24 +0900

    test: blog TOC index, mocking TOC useState

--- apps/blog/src/components/TOC/TOC.test.tsx
+++ apps/blog/src/components/TOC/TOC.test.tsx
@@ -12,9 +12,47 @@ jest.spyOn(React, 'useRef').mockImplementation(() => ({
 
 jest.spyOn(React, 'useEffect').mockImplementation(f => f());
 
+const TEST_HEADINGS = [
+  {
+    id: 'heading1',
+    text: 'heading1',
+  },
+  {
+    id: 'heading2',
+    text: 'some-thing! with @complex',
+  },
+];
+
+jest.mock('../../utils/getHeadings', () => {
+  return {
+    __esModule: true,
+    default: () => {
+      return TEST_HEADINGS;
+    },
+  };
+});
+
 describe('blog - TOC', () => {
   it('does not render when zero headings', () => {
     render(<TOC />);
     expect(screen.queryByText('Contents')).not.toBeInTheDocument();
   });
+
+  // eslint-disable-next-line jest/no-commented-out-tests
+  // it('should render contents when has headings', async () => {
+  //   Object.defineProperty(window, 'innerWidth', {
+  //     writable: true,
+  //     configurable: true,
+  //     value: 1500,
+  //   });
+
+  //   jest.spyOn(React, 'useState').mockReturnValue([TEST_HEADINGS, () => {}]);
+
+  //   const { rerender } = render(<TOC />);
+
+  //   rerender(<TOC />);
+  //   await waitFor(() => {
+  //     expect(screen.getByText('Contents')).toBeInTheDocument();
+  //   });
+  // });
 });
---
+++ apps/blog/src/components/TOC/index.test.tsx
@@ -0,0 +1,7 @@
+import Default from './index';
+
+describe('blog - components - TOC - index', () => {
+  it('should defined', () => {
+    expect(Default).toBeDefined();
+  });
+});
