4b7253cf300283cca4d87d4dda9385c6f5ecdb99
Hyesung Oh <haesungoh414@gmail.com>
2022-07-10 21:37:23 +0900

    chore: rename blog cypress file and directory

--- apps/blog/cypress/e2e/main.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('Main - Navigation', () => {
-  it('should navigate to the root page', () => {
-    cy.visit('/');
-    cy.get('h1').click();
-    cy.url().should('eq', Cypress.config().baseUrl);
-  });
-});
