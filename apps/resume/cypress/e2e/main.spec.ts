5e12d6ba9e8000f1ae190c3918ce018d7786bb27
Hyesung Oh <haesungoh414@gmail.com>
2022-07-13 23:53:20 +0900

    test: replace resume e2e test

--- apps/resume/cypress/e2e/main.spec.ts
+++
@@ -1,8 +0,0 @@
-import { data as headerData } from '../../_content/Header';
-
-describe('Main', () => {
-  it('should display header datas heading', () => {
-    cy.visit('/');
-    cy.get('h1').contains(headerData.heading);
-  });
-});
