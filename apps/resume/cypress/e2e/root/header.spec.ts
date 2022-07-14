aca861f58c6914daf5e0522da7e4ad4fe6aca536
hyesung oh <haesungoh414@gmail.com>
2022-07-14 21:08:34 +0900

    Apply theme toggle switch e2e test at resume (#94)
    * test: e2e resume - find theme toggle switch
    
    * test: e2e resume -  theme toggle switch work

--- apps/resume/cypress/e2e/root/header.spec.ts
+++ apps/resume/cypress/e2e/root/header.spec.ts
@@ -17,4 +17,27 @@ describe('root - header', () => {
   it('should display headerData description at header', () => {
     cy.get('header').find('p').should('be.visible');
   });
+
+  const THEME_SWITCH_LABEL = 'theme switch';
+  const GET_THEME_SWITCH_BY_LABEL = `[aria-label="${THEME_SWITCH_LABEL}"]`;
+
+  it('should visible theme toggle switch at header', () => {
+    cy.get('header').find(GET_THEME_SWITCH_BY_LABEL).should('be.visible');
+  });
+
+  it('should toggle theme when click theme toggle switch', () => {
+    // it means prefer light theme and width greater than 650px
+    cy.visit('/', {
+      onBeforeLoad: win => {
+        cy.stub(win, 'matchMedia').returns({ matches: false, addListener: () => {}, removeListener: () => {} });
+      },
+    });
+
+    cy.get('body').then($body => {
+      const lightBgColor = $body.css('background-color');
+
+      cy.get(GET_THEME_SWITCH_BY_LABEL).click();
+      cy.get('body').should('have.css', 'background-color').and('not.eq', lightBgColor);
+    });
+  });
 });
