e91bad5b573237186c08a3e17d66c3d110f8e4bc
Hyesung Oh <haesungoh414@gmail.com>
2022-07-15 22:14:18 +0900

    test: e2e blog - root footer

---
+++ apps/blog/cypress/e2e/root/footer.spec.tsx
@@ -0,0 +1,11 @@
+describe('root - footer', () => {
+  it('should render footer', () => {
+    cy.visit('/');
+    cy.scrollTo('bottom');
+    cy.get('footer').should('exist').and('be.visible');
+  });
+
+  it('should have anchor at footer', () => {
+    cy.get('footer').find('a').should('exist');
+  });
+});
