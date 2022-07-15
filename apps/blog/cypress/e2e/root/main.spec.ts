65a707bee0867b43b4e5ff8182576033882b10de
hyesung oh <haesungoh414@gmail.com>
2022-07-15 22:18:17 +0900

    Apply e2e test at blog root main (#96)

---
+++ apps/blog/cypress/e2e/root/main.spec.ts
@@ -0,0 +1,18 @@
+describe('root - main', () => {
+  it('should render level 3 heading inside of main', () => {
+    cy.visit('/');
+    cy.get('main').find('h3').should('exist');
+  });
+
+  it('should have anchor inside of main first article', () => {
+    cy.get('main').get('article').first().find('a').should('exist');
+  });
+
+  it('should have small inside of main first article', () => {
+    cy.get('main').get('article').first().find('small').should('exist');
+  });
+
+  it('should have anchor inside of small', () => {
+    cy.get('main').get('article').first().find('small').find('a').should('exist');
+  });
+});
