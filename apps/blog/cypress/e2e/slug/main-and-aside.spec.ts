3da3bafb4599bf8c483fe1bfb6708230c032e52e
hyesung oh <haesungoh414@gmail.com>
2022-07-17 13:11:29 +0900

    Apply e2e test at blog slug main and aside (#100)
    * test: apply e2e test at blog slug main and aside
    
    * test: set cypress command timeout to 30000

---
+++ apps/blog/cypress/e2e/slug/main-and-aside.spec.ts
@@ -0,0 +1,29 @@
+describe('slug - main', () => {
+  it('should linkable to slug', () => {
+    cy.visit('/');
+    cy.get('main').find('a').first().click();
+    cy.url().should('not.eq', Cypress.config().baseUrl);
+  });
+
+  it('should have level 1 heading', () => {
+    cy.get('h1').should('exist');
+  });
+
+  it('should exist category link at main', () => {
+    cy.get('main').find('p').first().find('a').should('exist');
+  });
+
+  it('should exist article at inside of main', () => {
+    cy.get('main').find('article').should('exist');
+  });
+
+  it('should render TOC when width size over 1000', () => {
+    cy.viewport('macbook-13');
+    cy.get('aside').should('exist');
+  });
+
+  it('should not render TOC when width size under 1000', () => {
+    cy.viewport('iphone-xr');
+    cy.get('aside').should('not.exist');
+  });
+});
