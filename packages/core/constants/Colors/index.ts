a80db8e7ae2d2f852473cb8fddab4873dd227ef3
Hyesung Oh <haesungoh414@gmail.com>
2022-08-04 16:38:18 +0900

    design: change dark theme background color

--- packages/core/constants/Colors/index.ts
+++ packages/core/constants/Colors/index.ts
@@ -10,4 +10,4 @@ export const darkPrimary: ColorType = null;
 export const darkSelection: ColorType = '#0070F3';
 export const darkCode: ColorType = '#6cc0e1';
 export const darkCodeLight: ColorType = '#16272e';
-export const darkBackground: ColorType = null;
+export const darkBackground: ColorType = '#202021';
