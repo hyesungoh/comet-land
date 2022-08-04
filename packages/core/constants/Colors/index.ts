e75d63bbddfbb54df95c7ec4b355dfbd19eb2bd4
Hyesung Oh <haesungoh414@gmail.com>
2022-08-04 16:33:35 +0900

    feat: apply background color and primary shadow color at colors

--- packages/core/constants/Colors/index.ts
+++ packages/core/constants/Colors/index.ts
@@ -4,8 +4,10 @@ export const lightPrimary: ColorType = null;
 export const lightSelection: ColorType = null;
 export const lightCode: ColorType = null;
 export const lightCodeLight: ColorType = null;
+export const lightBackground: ColorType = null;
 
 export const darkPrimary: ColorType = null;
 export const darkSelection: ColorType = '#0070F3';
 export const darkCode: ColorType = '#6cc0e1';
 export const darkCodeLight: ColorType = '#16272e';
+export const darkBackground: ColorType = null;
