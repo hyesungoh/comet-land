707cec9a444e90e224ed7d0ec8eaa858b52e611c
bong <48943350+the-bong@users.noreply.github.com>
2022-08-18 14:41:02 +0900

    fix: add jpeg to avoid test.

--- packages/config/jest.js
+++ packages/config/jest.js
@@ -5,7 +5,7 @@ module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(test))\\.[jt]sx?$',
   collectCoverageFrom: ['**/src/**/*.{js,ts,jsx,tsx}', '**/{components,hooks,utils}/**/*.{ts,tsx}'],
   moduleNameMapper: {
-    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|ico)$': 'identity-obj-proxy',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|jpeg|ttf|woff|woff2|ico)$': 'identity-obj-proxy',
   },
   moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
   transform: {
