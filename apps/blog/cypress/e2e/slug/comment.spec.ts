efcb3ead80b30cd90d13125f9fc226dc589e93ee
Hyesung Oh <haesungoh414@gmail.com>
2023-03-31 15:00:33 +0900

    test: append wait for utterances and scroll to bottom

--- apps/blog/cypress/e2e/slug/comment.spec.ts
+++ apps/blog/cypress/e2e/slug/comment.spec.ts
@@ -6,6 +6,9 @@ describe('slug - comment', { testIsolation: false }, () => {
   });
 
   it('should render iframe within utterances class', () => {
+    cy.wait(1000);
+    cy.scrollTo('bottom');
+
     cy.get('.utterances').within(() => {
       cy.get('iframe').should('be.visible');
     });
