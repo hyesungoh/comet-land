1de6cbbeeda99a7eb12cab5be84c82aa2a01dfed
Hyesung Oh <haesungoh414@gmail.com>
2023-03-31 14:53:09 +0900

    test: e2e test at slug's comment

---
+++ apps/blog/cypress/e2e/slug/comment.spec.ts
@@ -0,0 +1,17 @@
+describe('slug - comment', { testIsolation: false }, () => {
+  it('should link to slug page', () => {
+    cy.visit('/');
+    cy.get('main').find('a').first().click();
+    cy.url().should('not.eq', Cypress.config().baseUrl);
+  });
+
+  it('should render iframe within utterances class', () => {
+    cy.get('.utterances').within(() => {
+      cy.get('iframe').should('be.visible');
+    });
+  });
+
+  it('should have one utterances class', () => {
+    cy.get('.utterances').should('have.length', 1);
+  });
+});
