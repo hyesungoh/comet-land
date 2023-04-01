f2b090faa0d20c2c8499155cf146fd9513e7e754
Hyesung Oh <haesungoh414@gmail.com>
2023-04-01 16:33:58 +0900

    chore: upgrade deps include testing library

--- packages/config/jest.js
+++ packages/config/jest.js
@@ -1,8 +1,7 @@
-/** @type {import('jest').Config} */
 const customJestConfig = {
   resetMocks: true,
   moduleDirectories: ['node_modules'],
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
   testRegex: '(/__tests__/.*|(\\.|/)(test))\\.[jt]sx?$',
   collectCoverageFrom: ['**/src/**/*.{js,ts,jsx,tsx}', '**/{components,hooks,utils}/**/*.{ts,tsx}'],
   moduleNameMapper: {
@@ -18,24 +17,5 @@ const customJestConfig = {
   setupFilesAfterEnv: ['./jest.setup.js'],
   transformIgnorePatterns: [],
 };
-
-// {
-//   resetMocks: true,
-//   moduleDirectories: ['node_modules'],
-//   testEnvironment: 'jsdom',
-//   testRegex: '(/__tests__/.*|(\\.|/)(test))\\.[jt]sx?$',
-//   collectCoverageFrom: ['**/src/**/*.{js,ts,jsx,tsx}', '**/{components,hooks,utils}/**/*.{ts,tsx}'],
-//   moduleNameMapper: {
-//     '.+\\.(css|styl|less|sass|scss|png|jpg|jpeg|ttf|woff|woff2|ico)$': 'identity-obj-proxy',
-//   },
-//   moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
-//   transform: {
-//     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
-//   },
-//   coverageThreshold: null,
-//   setupFilesAfterEnv: ['./jest.setup.js'],
-//   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
-//   transformIgnorePatterns: ['/node_modules/jest-runner'],
-// };
 
 module.exports = customJestConfig;
