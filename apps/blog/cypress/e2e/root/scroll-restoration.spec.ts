2b149bdc61e50e3afb87eecf2d3dbf17dcf5fc9c
hyesungoh <haesungoh414@gmail.com>
2023-11-07 19:55:42 +0900

    test: update e2e test

--- apps/blog/cypress/e2e/root/scroll-restoration.spec.ts
+++ apps/blog/cypress/e2e/root/scroll-restoration.spec.ts
@@ -1,15 +1,19 @@
 describe('root - store restoration', { testIsolation: false }, () => {
-  it('should restore scroll position when reload', () => {
+  it('should restore scroll position when go back', () => {
     const SCROLL_Y_POSITION = 30;
 
     cy.visit('/');
-    cy.wait(1000);
+    cy.wait(500);
+
+    cy.get('main').get('article').first().find('a').first().click();
+    cy.wait(500);
+
     cy.window().then($window => {
       $window.scrollTo(0, SCROLL_Y_POSITION);
     });
 
-    cy.reload();
-    cy.wait(2000);
+    cy.go('back');
+    cy.wait(500);
 
     cy.window().its('scrollY').should('not.eq', 0);
   });
