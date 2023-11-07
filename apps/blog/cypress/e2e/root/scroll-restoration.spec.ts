16a82d2b0495661f54a2c8f477698a0987b747e7
hyesungoh <haesungoh414@gmail.com>
2023-11-08 00:25:50 +0900

    test: update e2e test with wait alias

--- apps/blog/cypress/e2e/root/scroll-restoration.spec.ts
+++ apps/blog/cypress/e2e/root/scroll-restoration.spec.ts
@@ -2,19 +2,19 @@ describe('root - store restoration', { testIsolation: false }, () => {
   it('should restore scroll position when go back', () => {
     const SCROLL_Y_POSITION = 30;
 
+    cy.intercept('/').as('visitHome');
     cy.visit('/');
-    cy.wait(500);
+    cy.wait('@visitHome');
 
     cy.get('main').get('article').first().find('a').first().click();
-    cy.wait(500);
+    cy.intercept(/\/\w+/).as('visitArticle');
+    cy.wait('@visitArticle');
 
     cy.window().then($window => {
       $window.scrollTo(0, SCROLL_Y_POSITION);
     });
 
     cy.go('back');
-    cy.wait(500);
-
     cy.window().its('scrollY').should('not.eq', 0);
   });
 });
