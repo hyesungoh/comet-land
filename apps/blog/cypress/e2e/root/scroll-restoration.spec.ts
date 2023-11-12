a0144cd67ab9fae89b387fd68d300cdd80c4c12b
hyesungoh <haesungoh414@gmail.com>
2023-11-12 18:10:18 +0900

    test: delete wait

--- apps/blog/cypress/e2e/root/scroll-restoration.spec.ts
+++ apps/blog/cypress/e2e/root/scroll-restoration.spec.ts
@@ -2,19 +2,14 @@ describe('root - store restoration', { testIsolation: false }, () => {
   it('should restore scroll position when go back', () => {
     const SCROLL_Y_POSITION = 30;
 
-    cy.intercept('/').as('visitHome');
     cy.visit('/');
-    cy.wait('@visitHome');
-
     cy.window().then($window => {
       $window.scrollTo(0, SCROLL_Y_POSITION);
     });
 
     cy.get('main').find('a').first().click();
-    cy.wait(3000);
 
-    cy.go('back');
-    cy.wait(3000);
+    cy.go('back', { timeout: 10000 });
 
     cy.window().its('scrollY').should('not.eq', 0);
   });
