3f24a02ef07722a10145e48830f0fd61e636b2b2
hyesungoh <haesungoh414@gmail.com>
2023-11-12 18:38:01 +0900

    test: disable test

--- apps/blog/cypress/e2e/root/scroll-restoration.spec.ts
+++ apps/blog/cypress/e2e/root/scroll-restoration.spec.ts
@@ -1,16 +1,18 @@
-describe('root - store restoration', { testIsolation: false }, () => {
-  it('should restore scroll position when go back', () => {
-    const SCROLL_Y_POSITION = 30;
+// NOTE: https://github.com/cypress-io/cypress/issues/14857
 
-    cy.visit('/');
-    cy.window().then($window => {
-      $window.scrollTo(0, SCROLL_Y_POSITION);
-    });
+// describe('root - store restoration', { testIsolation: false }, () => {
+//   it('should restore scroll position when go back', () => {
+//     const SCROLL_Y_POSITION = 30;
 
-    cy.get('main').find('a').first().click();
+//     cy.visit('/');
+//     cy.window().then($window => {
+//       $window.scrollTo(0, SCROLL_Y_POSITION);
+//     });
 
-    cy.go('back', { timeout: 10000 });
+//     cy.get('main').find('a').first().click();
 
-    cy.window().its('scrollY').should('not.eq', 0);
-  });
-});
+//     cy.go('back', { timeout: 10000 });
+
+//     cy.window().its('scrollY').should('not.eq', 0);
+//   });
+// });
